3717b0cf29c337cf4d37968927e885faaa1bbcf2
ArvinDing <dingarvin@gmail.com>
2023-11-20 18:18:02 -0800

    added force condition

---
+++ force.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int TROOPS = 100;
+bool win[TROOPS + 1][TROOPS + 1][5] { };
+int bestChoice[TROOPS + 1][TROOPS + 1][5] { };
+bool isWin(int yourTroops, int oppTroops, int score) {
+	if (score >= 3)
+		return true;
+	if (score <= -3)
+		return false;
+	return win[yourTroops][oppTroops][score + 2];
+}
+int main() {
+	for (int yourTroops = 0; yourTroops <= TROOPS; yourTroops++) {
+		for (int oppTroops = 0; oppTroops <= TROOPS; oppTroops++) {
+			for (int score = -2; score <= 2; score++) {
+
+				for (int choose = 1; choose <= yourTroops; choose++) {
+					bool good = true;
+
+					for (int oppChoice = 0; oppChoice <= oppTroops;
+							oppChoice++) {
+						int scoreDiff = 0;
+						if (choose < oppChoice)
+							scoreDiff--;
+						if (choose > oppChoice)
+							scoreDiff++;
+						if (!isWin(yourTroops - choose, oppTroops - oppChoice,
+								score + scoreDiff)) {
+							good = false;
+						}
+					}
+					if (good) {
+						if (score != 2) {
+							cout << yourTroops << " " << oppTroops << " "
+									<< score << "\n";
+							cout << choose
+									<< "\n";
+						}
+						win[yourTroops][oppTroops][score + 2] = true;
+						bestChoice[yourTroops][oppTroops][score + 2] = choose;
+						break;
+					}
+				}
+
+			}
+		}
+	}
+}
